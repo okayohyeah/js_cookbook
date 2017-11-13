69f1494b3a8dc2562d4ca40f4ccc65e9bf89f6f6
Katherine Oh <hello@katherineoh.com>
2017-11-12 16:05:29 -0800

    recipe 1.2: added test code, alternative string extraction method, and comments

--- chap1/recipe1-2.js
+++ chap1/recipe1-2.js
@@ -1,10 +1,45 @@
 var sentence = 'This is one sentence. This is a sentence with a list of items:' + 
 'cherries, oranges, apples, bananas. That was the list of items.';
-var start = sentence.indexOf(':');
-var end = sentence.indexOf('.', start+1);
+console.log(sentence); 
+//This is one sentence. This is a sentence with a list of items:cherries, oranges, apples, bananas. That was the list of items.
 
-var listStr = sentence.substring(start+1, end);
 
-var fruits = listStr.split(','); 
+//indexOf() method takes the search value of the first parameter (with optional beginning index position, as second parameter)
+var start = sentence.indexOf(':'); //String indexOf() to locate the colon 
+console.log(start); //61
+
+var end = sentence.indexOf('.', start+1); //Searching for period: String indexOf() to locate the second period, after the colon. The colon's position (61) plus 1 is used to modify the beginning location of the search (62) for the period. 
+console.log(end); //96
+
+
+
+//substring() method takes two index values representing the beginning and ending position of the string
+var listStr = sentence.substring(start+1, end); //Locate the list: String substring() to extract using the beginning position (start+1=62) and ending position (96)
+console.log(listStr); //cherries, oranges, apples, bananas
+
+
+
+//split() method used to split a string into an array of substrings and returns the new array.  
+var fruits = listStr.split(','); //String split() to break the string into an array - split the string at each point at the separator, in this case we specified the separator as ',' 
 console.log(fruits); // ['cherries', ' oranges', ' apples', ' bananas']
+
+
+
+//OPTION 2 string extraction method
+//substring() method takes two index values representing the beginning and ending position of the string
+var listStr = sentence.substring(start+1, end); //Locate the list: String substring() to extract using the beginning position (start+1=62) and ending position (96)
+console.log(listStr); //cherries, oranges, apples, bananas
+
+//substr() method takes two index values. The first index position marks the start of the substring and passing in the length of the substring as the second parameter.
+var listStr2 = sentence.substr(start+1, end-start); //first index position 62: start of the substring
+																										//second index position 35: length of substring
+console.log(listStr2); //cherries, oranges, apples, bananas
+console.log(start+1); //62
+console.log(end-start); //35
+
+//split() method used to split a string into an array of substrings and returns the new array.  
+var fruits = listStr2.split(','); //String split() to break the string into an array - split the string at each point at the separator, in this case we specified the separator as ',' 
+console.log(fruits); // ['cherries', ' oranges', ' apples', ' bananas']
+
+
 
