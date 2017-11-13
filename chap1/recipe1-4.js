b9f4404e519466d02b4d921066f2df3d483c3c35
Katherine Oh <hello@katherineoh.com>
2017-11-12 21:29:12 -0800

    recipe1.4: inserting special characters

---
+++ chap1/recipe1-4.js
@@ -0,0 +1,9 @@
+//Inserting Special Characters
+var resultString = 'This page \u00A0 Katherine Oh'; 
+
+var newString = 'Here\'s a single quote, a \"double quote\" , \\ is a backslash, \n started a new line';
+// \' adds a single quote
+// \" adds a double quote
+// \\ adds a backslash
+// \b backspace
+// \u00A0 adds Copyright symbol
