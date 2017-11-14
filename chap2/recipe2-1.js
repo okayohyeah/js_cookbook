f4b0f1b85b28ed303f7f864a1e2ce06c417758b8
Katherine Oh <hello@katherineoh.com>
2017-11-14 13:30:31 -0800

    recipe2.1: addcode for searching arrays w/comments

--- chap2/recipe2-1.js
+++ chap2/recipe2-1.js
@@ -1,14 +1,12 @@
 // Searching Through an Array
 
 // no array data type in JS but there are JS Array objects
-var animals = new Array("dog","cat","seal","elephant","walrus","elephant","lion","cat");
+var animals = ["dog", "cat", "seal", "elephant", "walrus", "elephant", "lion", "cat"];
 
-//Array method indexOf() takes in a search value and returns the first one found. 
-		//If found within array, returns an index number represening the array element. If the value is NOT found, -1 is returned.
+//Array method indexOf() takes in a search value and returns the first one found.****If found within array, returns an index number represening the array element. If the value is NOT found, -1 is returned.
 console.log(animals.indexOf("elephant")); //  3
 
-//Array method lastIndexOf() takes in a search value and returns the last one found. 
-		//If found within array, returns an index number represening the array element. If the value is NOT found, -1 is returned.
+//Array method lastIndexOf() takes in a search value and returns the last one found.****If found within array, returns an index number represening the array element. If the value is NOT found, -1 is returned.
 console.log(animals.lastIndexOf("cat")); // 7
 
 
@@ -16,11 +14,11 @@ var cars = ["element", "camry", "escort", "sonata", "camry", "corolla"];
 
 // indexOf(value, starting index)
 //find value "camry", start search at index 2 and find index of first one
-console.log(cars.indexOf("camry",2));  // 4
+console.log(cars.indexOf("camry", 2));  // 4
 
 // lastIndexOf(value, starting index)
 //find value "camry", start search at index 2 and find index of last one
-console.log(cars.lastIndexOf("camry",5));  // 4
+console.log(cars.lastIndexOf("camry", 5));  // 4
 
 
 
@@ -29,8 +27,8 @@ console.log(cars.lastIndexOf("camry",5));  // 4
 var nums = [2, 4, 19, 15, 183, 6, 7, 1, 1];
 
 // findIndex() method, providing a function that tests each array value, and returns the index of the array element when the test is successful
-var over = nums.findIndex(function(element) {
-	return (element >= 100);
+var over = nums.findIndex(function (element) {
+return (element >= 100);
 });
 
 console.log(over);  // 4
