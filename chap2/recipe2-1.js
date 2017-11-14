9f269551a10da37eee8df2d868e1008053db6a2e
Katherine Oh <hello@katherineoh.com>
2017-11-13 16:40:52 -0800

    WIP recipe2.1

--- chap2/recipe2-1.js
+++ chap2/recipe2-1.js
@@ -1,3 +1,12 @@
-var animals = new Array("dog","cat","seal","elephant","walrus","lion");
-console.log(animals.indexOf("elephant")); // prints 3
+// Searching Through an Array
+
+// no array data type in JS but there are JS Array objects
+var animals = new Array("dog","cat","seal","elephant","walrus","elephant","lion","cat");
+
+//Array method indexOf() takes in a search value and returns the first one found. 
+		//If found within array, returns an index number represening the array element. If the value is NOT found, -1 is returned.
+console.log(animals.indexOf("elephant")); //  3
+//Array method lastIndexOf() takes in a search value and returns the last one found. 
+		//If found within array, returns an index number represening the array element. If the value is NOT found, -1 is returned.
+console.log(animals.lastIndexOf("cat")); // 7
 
