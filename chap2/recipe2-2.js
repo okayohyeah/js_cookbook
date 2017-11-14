f3f9b70bbafddab1e0b7643f415d72dd9437fbd8
Katherine Oh <hello@katherineoh.com>
2017-11-14 15:53:25 -0800

    WIP recipe2.2

--- chap2/recipe2-2.js
+++ chap2/recipe2-2.js
@@ -1,9 +1,12 @@
+// parent array
 var fruitarray = [];
-fruitarray[0] = ['strawberry','orange'];
-fruitarray[1] = ['lime','peach','banana'];
-fruitarray[2] = ['tangerine','apricot'];
-fruitarray[3] = ['raspberry','kiwi'];
+// multi-dimensional arrays
+fruitarray[0] = ['strawberry', 'orange'];
+fruitarray[1] = ['lime', 'peach', 'banana'];
+fruitarray[2] = ['tangerine', 'apricot'];
+fruitarray[3] = ['raspberry', 'kiwi'];
 
 // flatten array
-var newArray = fruitarray.concat.apply([],fruitarray);
+// Array object concat() method takes one or more arrays and appends the array elements on to the end of the contents of the parent array on which the method was called.
+var newArray = fruitarray.concat.apply([], fruitarray);
 console.log(newArray[5]); // tangerine
