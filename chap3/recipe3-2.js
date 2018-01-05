5dcb4156b194a3709e074961ad32501d0a3260ae
Katherine Oh <hello@katherineoh.com>
2018-01-05 13:32:11 -0800

    recipe 3-2: passing function as an argument to another function exercise

--- chap3/recipe3-2.js
+++ chap3/recipe3-2.js
@@ -4,9 +4,21 @@ var reverseArray = function(x,indx,str) {
 }
 
 var arr = ['apple','orange','peach','lime'];
-var str = reverseArray(arr,arr.length,"");
-console.log(str);
+var str = reverseArray(arr,arr.length,""); 
+	// reverseArray(['apple','orange','peach','lime'], 4, "")
+	// indx=4 -> reverseArray(['apple','orange','peach','lime'], --indx = 3, arr[3]= lime)
+	// indx=3 ->  							 																--indx = 2, arr[2]= peach
+	// indx=2 ->  							 																--indx = 1, arr[2]= orange
+	// indx=1 ->  							 																--indx = 0, arr[2]= apple
+	// indx=0 -> str = " lime peach orange apple	"																				
+console.log(str); // lime peach orange apple
 
 var arr2 = ['car','boat','sun','computer'];
 str = reverseArray(arr2,arr2.length,"");
-console.log(str);
+	// reverseArray(['car','boat','sun','computer'], 4, "")
+	// indx=4 -> reverseArray(['car','boat','sun','computer'], --indx = 3, arr[3]= computer)
+	// indx=3 ->																							--indx = 2, arr[2]= sun
+	// indx=2 ->																							--indx = 1, arr[2]= boat
+	// indx=1 ->																							--indx = 2, arr[2]= car
+	// indx=0 -> str = " computer sun boat car"
+console.log(str); // computuer sun boat car
